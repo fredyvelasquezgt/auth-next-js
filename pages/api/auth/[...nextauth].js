f477df6cde8ec92103f75ec8919ebb574791041e
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-24 22:16:35 -0600

    setting up credentials

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,20 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import { connectToDatabase } from "../../../lib/db";
 
 export default NextAuth({
-    providers: []
+    providers: [
+        Providers.Credentials({
+            async authorize(credentials) {
+                const client = await connectToDatabase()
+                
+                const usersCollection = client.db.collections('users');
+
+                usersCollection.findOne({email: credentials.email})
+
+
+                client.close();
+            }
+        })
+    ]
 });
