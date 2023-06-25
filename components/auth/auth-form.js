4c02256a10ecb2a45680e38db30fd04b621a12a4
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-24 21:39:50 -0600

    build post request

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,11 +1,40 @@
 import { useState } from 'react';
 import classes from './auth-form.module.css';
 
+function createdUser(email, password) {
+  const response = await fetch('/api/auth/signup', {
+    method: 'POST',
+    body: JSON.stringify({email, password}),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+
+  const data = await response.json();
+
+  if(!response.ok) {
+    throw new Error(data.message || 'Error!!!')
+  }
+
+  return data;
+
+}
+
 function AuthForm() {
   const [isLogin, setIsLogin] = useState(true);
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    if(isLogin) {
+
+    } else {
+      createdUser();
+    }
   }
 
   return (
