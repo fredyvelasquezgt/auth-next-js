a9f12324a831b7de5e1822b3ca90e233f2527b59
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-24 21:15:07 -0600

    validate data

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,15 +1,24 @@
 import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
+  const data = req.body;
 
-    const data = req.body;
+  const { email, password } = data;
 
-    const {email, password} = data; 
+  if (
+    !email ||
+    !email.inclues("@") ||
+    !password ||
+    password.trim().lenght < 7
+  ) {
+    res.status(422).json({ message: "invalid input" });
+    return;
+  }
 
-   const client = await  connectToDatabase()
-   const db = client.db()
+  const client = await connectToDatabase();
+  const db = client.db();
 
-   db.collection('users')
+  db.collection("users");
 }
 
 export default hanlder;
