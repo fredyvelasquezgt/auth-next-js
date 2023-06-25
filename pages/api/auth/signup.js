9dba8f498a066910cfad2d469d71d1660beb778d
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-24 21:46:04 -0600

    refactor request type

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -3,35 +3,35 @@ import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
 
-    if(req.method === 'POST') {
-        const data = req.body;
-
-        const { email, password } = data;
-      
-        if (
-          !email ||
-          !email.inclues("@") ||
-          !password ||
-          password.trim().lenght < 7
-        ) {
-          res.status(422).json({ message: "invalid input" });
-          return;
-        }
-      
-        const client = await connectToDatabase();
-        const db = client.db();
-      
-        const hashedPassword = hashPassword(password);
-      
-        const result = await db.collection('users').insertOne({
-            email: email,
-            password: password
-        })
-      
-        res.status(201).json({message: 'created user'})
-      }
+    if(req.method !== 'POST') {
+        return ;
     }
 
- 
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.inclues("@") ||
+    !password ||
+    password.trim().lenght < 7
+  ) {
+    res.status(422).json({ message: "invalid input" });
+    return;
+  }
+
+  const client = await connectToDatabase();
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = await db.collection('users').insertOne({
+      email: email,
+      password: password
+  })
+
+  res.status(201).json({message: 'created user'})
+}
 
 export default hanlder;
