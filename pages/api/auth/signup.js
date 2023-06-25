417eabad6eaf07f163a93e943879233ffa4d2a66
Fredy Velasquez <macacoloco696@gmail.com>
2023-06-24 21:11:14 -0600

    using connection function

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,5 +1,8 @@
-function hanlder() {
+import { connectToDatabase } from "../../../lib/db";
 
+async function ahanlder() {
+   const client = await  connectToDatabase()
+   const db = client.db()
 }
 
 export default hanlder;
